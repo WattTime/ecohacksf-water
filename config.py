f18bb9bb8d4eabb0e89803c46e5786d301f27604
Ashley Luhrs <aluhrs@gmail.com>
2014-05-10 18:12:55 -0700

    added spacing

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 DB_URI = os.environ.get("DATABASE_URL", "postgres://localhost/watttime")
 SECRET_KEY = os.environ.get("SECRET_KEY")
-TWILIO_ACCOUNT_SID=os.environ.get("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN=os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
